1591ec75a405da9de74e3a2a1c74bee0f3b68741
picel <picel@picel.net>
2022-10-06 11:29:17 +0900

    시계 기능 추가

--- clock/script.js
+++ clock/script.js
@@ -1,3 +1,7 @@
+let wallpaperSelected = false;
+let clockSelected = false;
+let is12Hour = false;
+
 function init() {
     let date = new Date();
     document.getElementById('hr').innerHTML = formatTime(date.getHours());
@@ -9,7 +13,11 @@ function init() {
 
 function timeCheck() {
     data = new Date();
-    document.getElementById('hr').innerHTML = formatTime(data.getHours());
+    if (is12Hour) {
+        document.getElementById('hr').innerHTML = formatTime(data.getHours() % 12);
+    } else {
+        document.getElementById('hr').innerHTML = formatTime(data.getHours());
+    }
     document.getElementById('min').innerHTML = formatTime(data.getMinutes());
     document.getElementById('sec').innerHTML = formatTime(data.getSeconds());
 }
@@ -22,4 +30,74 @@ function formatTime(time) {
     }
 }
 
+function wallpaperPicker() {
+    if (!(!document.getElementsByClassName('colorPickerBTN')[0].hasAttribute('hidden') && clockSelected)) {
+        document.getElementsByClassName('colorPickerBTN')[0].toggleAttribute('hidden');
+    }
+    wallpaperSelected = true;
+    clockSelected = false;
+    document.getElementById('nameOfButton').innerHTML = 'Wallpaper';
+}
+
+function clockPicker() {
+    if (!(!document.getElementsByClassName('colorPickerBTN')[0].hasAttribute('hidden') && wallpaperSelected)) {
+        document.getElementsByClassName('colorPickerBTN')[0].toggleAttribute('hidden');
+    }
+    clockSelected = true;
+    wallpaperSelected = false;
+    document.getElementById('nameOfButton').innerHTML = 'Clock';
+}
+
+function redBTN() {
+    if (wallpaperSelected) {
+        document.body.style.background = 'red';
+    } else if (clockSelected) {
+        document.getElementsByClassName('clock')[0].style.color = 'red';
+    }
+}
+
+function greenBTN() {
+    if (wallpaperSelected) {
+        document.body.style.background = 'green';
+    } else if (clockSelected) {
+        document.getElementsByClassName('clock')[0].style.color = 'green';
+    }
+}
+
+function blueBTN() {
+    if (wallpaperSelected) {
+        document.body.style.background = 'blue';
+    } else if (clockSelected) {
+        document.getElementsByClassName('clock')[0].style.color = 'blue';
+    }
+}
+
+function twelveHour() {
+    is12Hour = true;
+    document.getElementById('hr').innerHTML = formatTime(data.getHours() % 12);
+    document.getElementById("ampm").innerHTML = data.getHours() >= 12 ? "PM" : "AM";
+    document.getElementById("12").classList.remove("btn-secondary");
+    document.getElementById("12").classList.add("btn-primary");
+    document.getElementById("24").classList.remove("btn-primary");
+    document.getElementById("24").classList.add("btn-secondary");
+}
+
+function twentyFourHour() {
+    is12Hour = false;
+    document.getElementById('hr').innerHTML = formatTime(data.getHours());
+    document.getElementById("ampm").innerHTML = "";
+    document.getElementById("12").classList.remove("btn-primary");
+    document.getElementById("12").classList.add("btn-secondary");
+    document.getElementById("24").classList.remove("btn-secondary");
+    document.getElementById("24").classList.add("btn-primary");
+}
+
 init();
+
+document.getElementById("12").addEventListener("click", twelveHour);
+document.getElementById("24").addEventListener("click", twentyFourHour);
+document.getElementById('wallpaperBTN').addEventListener('click', wallpaperPicker);
+document.getElementById('clockBTN').addEventListener('click', clockPicker);
+document.getElementById('redBTN').addEventListener('click', redBTN);
+document.getElementById('blueBTN').addEventListener('click', blueBTN);
+document.getElementById('greenBTN').addEventListener('click', greenBTN);
