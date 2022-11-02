15477c5aa92751fcd75d121d17a37f79b9a36d9d
picel <picel@picel.net>
2022-11-02 12:12:00 +0900

    add scroll navbar (10)

--- 10/script.js
+++ 10/script.js
@@ -9,6 +9,17 @@ let dPOS = document.querySelector("#d").offsetTop;
 let buttons = document.querySelectorAll('.appBar__nav');
 
 buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        if (button.innerHTML == "a") {
+            window.scrollTo(0, aPOS);
+        } else if (button.innerHTML == "b") {
+            window.scrollTo(0, bPOS);
+        } else if (button.innerHTML == "c") {
+            window.scrollTo(0, cPOS);
+        } else if (button.innerHTML == "d") {
+            window.scrollTo(0, dPOS);
+        }
+    });
 });
 
 window.onscroll = function() {
@@ -16,6 +27,27 @@ window.onscroll = function() {
     scrollPosition = window.pageYOffset;
     if (scrollPosition > 0) {
         appBar.classList.remove('hidden');
+        if (scrollPosition >= aPOS && scrollPosition < bPOS) {
+            for (let i = 0; i < buttons.length; i++) {
+                buttons[i].classList.remove('appBar__selected');
+            }
+            buttons[0].classList.add('appBar__selected');
+        } else if (scrollPosition >= bPOS && scrollPosition < cPOS) {
+            for (let i = 0; i < buttons.length; i++) {
+                buttons[i].classList.remove('appBar__selected');
+            }
+            buttons[1].classList.add('appBar__selected');
+        } else if (scrollPosition >= cPOS && scrollPosition < dPOS) {
+            for (let i = 0; i < buttons.length; i++) {
+                buttons[i].classList.remove('appBar__selected');
+            }
+            buttons[2].classList.add('appBar__selected');
+        } else if (scrollPosition >= dPOS) {
+            for (let i = 0; i < buttons.length; i++) {
+                buttons[i].classList.remove('appBar__selected');
+            }
+            buttons[3].classList.add('appBar__selected');
+        }
     } else {
         appBar.classList.add('hidden');
     }
