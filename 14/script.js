09e3ae57487f200a3ff721c44a312d1b2b3624a5
picel <picel@picel.net>
2022-11-16 17:37:00 +0900

    Add edit function

--- 14/script.js
+++ 14/script.js
@@ -1,5 +1,11 @@
 let input = document.getElementById("input");
 let toDoList = document.getElementById("toDoList");
+let delButton = document.querySelectorAll(".del");
+let editButton = document.getElementById("editBtn");
+let editInput = document.getElementById("editForm");
+let modal = document.getElementById("modal");
+let closeBtn = document.getElementById("closeBtn");
+let editTarget;
 
 let TODOLIST = "todolist";
 
@@ -13,11 +19,21 @@ function load() {
         let tr = document.createElement("tr");
         let td1 = document.createElement("td");
         let td2 = document.createElement("td");
+        let del = document.createElement("i");
+        let edit = document.createElement("i");
         td1.innerHTML = tasks[i];
-        td2.innerHTML = "X";
-        td2.onclick = removeTask(tr);
+        del.innerHTML = "delete";
+        del.classList.add("material-icons");
+        del.classList.add("del");
+        edit.innerHTML = "edit";
+        edit.classList.add("material-icons");
+        edit.classList.add("edit");
+        td2.appendChild(del);
+        td2.appendChild(edit);
         tr.appendChild(td1);
         tr.appendChild(td2);
+        del.onclick = removeTask(tr);
+        edit.onclick = showModal(tr);
         toDoList.appendChild(tr);
     }
 }
@@ -30,6 +46,9 @@ function save() {
     localStorage.setItem(TODOLIST, JSON.stringify(tasks));
 }
 
+
+
+
 function addTask() {
     let task = input.value;
     if (task === "") {
@@ -38,21 +57,57 @@ function addTask() {
     let tr = document.createElement("tr");
     let td1 = document.createElement("td");
     let td2 = document.createElement("td");
+    let del = document.createElement("i");
+    let edit = document.createElement("i");
     td1.innerHTML = task;
-    td2.innerHTML = "X";
-    td2.onclick = removeTask(tr);
+    del.innerHTML = "delete";
+    del.classList.add("material-icons");
+    del.classList.add("del");
+    edit.innerHTML = "edit";
+    edit.classList.add("material-icons");
+    edit.classList.add("edit");
+    td2.appendChild(del);
+    td2.appendChild(edit);
     tr.appendChild(td1);
     tr.appendChild(td2);
+    del.onclick = removeTask(tr);
+    edit.onclick = showModal(tr);
     toDoList.appendChild(tr);
+
+
     input.value = "";
     save();
 }
 
 function removeTask(tr) {
     return function () {
-        toDoList.removeChild(tr);
+        tr.remove();
         save();
     }
+}
+
+function showModal(tr) {
+    return function () {
+        modal.hidden = false;
+        document.getElementById("background").classList.remove("blurOff");
+        document.getElementById("background").classList.add("blurOn");
+        editTarget = tr;
+        editInput.value = tr.children[0].innerHTML;
+    }
+}
+
+function closeModal() {
+    modal.hidden = true;
+    document.getElementById("background").classList.remove("blurOn");
+    document.getElementById("background").classList.add("blurOff");
+}
+
+closeBtn.onclick = closeModal;
+
+editButton.onclick = function () {
+    editTarget.children[0].innerHTML = editInput.value;
+    closeModal();
+    save();
 }
 
 window.onload = load;
