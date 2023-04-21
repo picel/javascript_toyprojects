ed397cd9ad5a730c937c73ea081303a36007c026
picel <picel@picel.net>
2023-04-21 11:45:48 +0900

    fix output

--- bionic_reading/script.js
+++ bionic_reading/script.js
@@ -1,27 +1,33 @@
 function capitalize() {
-    var text = document.getElementById("input").value;
-    var words = text.split(" ");
-    var result = "";
-    
-    for (var i = 0; i < words.length; i++) {
-        var word = words[i];
-        if (word.length < 2) {
-            result += word + " ";
-            continue;
-        } else if (word.length == 2) {
-            var firstLetter = word.charAt(0);
-            firstLetter = "<span class='accent'>" + firstLetter + "</span>";
-            result += firstLetter + word.charAt(1) + " ";
-        } else {
-            var firstLetter = word.charAt(0);
-            var secondLetter = word.charAt(1);
-            var rest = word.slice(2);
-            // make it red
-            firstLetter = "<span class='accent'>" + firstLetter + "</span>";
-            secondLetter = "<span class='accent'>" + secondLetter + "</span>";
-            result += firstLetter + secondLetter + rest + " ";
+    let text = document.getElementById("input").value;
+    let result = "";
+    // split the text into paragraphs
+    let paragraph = text.split("\n");
+    console.log(paragraph);
+    for (let i = 0; i < paragraph.length; i++) {
+        let words = paragraph[i].split(" ");
+        for (let j = 0; j < words.length; j++) {
+            let word = words[j];
+            if (word.length < 2) {
+                result += word + " ";
+                continue;
+            } else if (word.length == 2) {
+                let firstLetter = word.charAt(0);
+                firstLetter = "<span class='accent'>" + firstLetter + "</span>";
+                result += firstLetter + word.charAt(1) + " ";
+            } else {
+                let firstLetter = word.charAt(0);
+                let secondLetter = word.charAt(1);
+                let rest = word.slice(2);
+                // make it red
+                firstLetter = "<span class='accent'>" + firstLetter + "</span>";
+                secondLetter = "<span class='accent'>" + secondLetter + "</span>";
+                result += firstLetter + secondLetter + rest + " ";
+            }
         }
+        result += "<br>";
     }
+    console.log(result);
     let target = document.getElementById("output");
     target.innerHTML = result;
     target.hidden = false;
